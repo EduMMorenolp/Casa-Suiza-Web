47c44d94c79b5e56dcf7ddf657879cb950cf2272
Edu Moreno <e.m.morenolp@gmail.com>
2025-07-03 23:27:05 -0300

    error titulo

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -46,9 +46,6 @@ export default function Home() {
     <>
       <Header />
       <main className="container mx-auto p-6">
-        <h2 className="text-3xl font-bold text-darkBlue mb-6 text-center">
-          Próximos Eventos
-        </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {events.map((event, index) => (
             <EventCard key={index} event={event} />
