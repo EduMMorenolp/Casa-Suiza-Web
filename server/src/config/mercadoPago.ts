a8655039df16e870abaefca720f21525854db84f
Edu Moreno <e.m.morenolp@gmail.com>
2025-07-30 00:54:24 -0300

    fix

--- server/src/config/mercadoPago.ts
+++ server/src/config/mercadoPago.ts
@@ -4,8 +4,8 @@ import { MercadoPagoConfig, Payment, Preference } from "mercadopago";
 // Reemplaza 'TU_ACCESS_TOKEN_AQUI' con process.env.MERCADO_PAGO_ACCESS_TOKEN
 const client = new MercadoPagoConfig({
   accessToken:
-    process.env.MERCADO_PAGO_ACCESS_TOKEN ||
-    "APP_USR-6516061950356800-072717-dd6c3f83cc861fa233646c5441aac4d9-2583119160",
+    process.env.MP_ACCESS_TOKEN ||
+    "TEST-6516061950356800-072717-dd6c3f83cc861fa233646c5441aac4d9-2583119160",
 });
 
 export const mpPayment = new Payment(client);
